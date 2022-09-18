4af6f3880c8dba20a7971860c3f60e21652e9a58
PengyuWu5 <frankwu@bu.edu>
2022-09-18 19:12:01 -0400

    Added Alert system, input option
    included a title at top and a confirm button connect to the input option.
    Also, trigger alert when input is empty.

--- App.js
+++ App.js
@@ -1,33 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image,TouchableOpacity, Button, View } from 'react-native';
+import { StyleSheet, Text, Image,TouchableOpacity, Button, View, Alert } from 'react-native';
 //import { Button } from 'react-native-web';
-
+import  Top from './Components/toppart'
+import EnterAccount from './Components/EnterAccount';
+ 
 export default function App() {
-  return (
+
+  const confirmHandle = (InputText) =>{
+  //connect to twitter and pull the data to detect bot
+  //Implement here
+
+  //Alert when input is empty
+  if(InputText.length < 1){
+    Alert.alert('Please Note',
+      'Account Name can not be empty!',
+    [{text: 'OK', onPress: () => console.log('alert closed')}])
+  }
+  //Alert when account does not exist
+  //implement here
+
+  }
+  return (      
     <View style={styles.container}>
-      <Text>Welcome to the Twiter Bot Detecter</Text>
+      <Top style = {styles.Header}/>
+      
       <Image source={require("./assets/background2.png")} style={styles.BackgroundImage}/> 
-           <Button 
-           title="Enter your account name" 
-           color ='red'
-           onPress={() => console.log("Button tapped")}
-            />
+      <EnterAccount confirmHandle ={confirmHandle}/>
+           
       <StatusBar style="auto" />
     </View>
+    
    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00bfff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   //Background Image Size and Position
   BackgroundImage:{
+    marginTop: 20,
     width:80,
     height:80,
+  },
+  Header:{
+    top:0
+  },
+  ConfirmButton:{
+    justifyContent: 'center',
+    marginTop: 100,
+    fontWeight:'bold'
   }
 });
 
