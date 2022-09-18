9a1e86f1b39d8878321b94e9babf00ad0a74c486
PengyuWu5 <frankwu@bu.edu>
2022-09-18 19:12:01 -0400

    Added Alert system, input option
    included a title at top and a confirm button connect to the input option.
    Also, trigger alert when input is empty.

---
+++ Components/EnterAccount.js
@@ -0,0 +1,39 @@
+//import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, Image,TouchableOpacity, Button, View, TextInput} from 'react-native';
+
+//Allow user to enter account names
+export default function EnterAccount({confirmHandle}) {
+    const[defaultText, EnterText] = useState('');
+
+    const redirect = (val) =>(
+        EnterText(val)
+    )
+    return(
+     <View style={styles.EnterAccount}>
+         <TextInput 
+         style = {styles.enter}
+         placeholder ='Enter the Account Name...'
+         onChangeText={redirect}>
+         </TextInput>
+         <Button 
+           style = {styles.ConfirmButton}
+           title="Confirm" 
+           color ='red'
+           onPress={() => confirmHandle(defaultText)}
+            />
+      </View>
+    )
+ } 
+
+ const styles = StyleSheet.create({
+    enter:{
+        textAlign: 'center',
+        fontSize:'16',
+        marginBottom: 10,
+        marginTop:100,
+        borderBottomWidth: 1,
+        borderBottomColor: 'white'
+        //fontweight:'bold'
+    }
+})
