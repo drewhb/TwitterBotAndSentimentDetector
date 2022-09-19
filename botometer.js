cee1738a4062338ea6a4c99e5c790f885e479aff
drewhb <74934734+drewhb@users.noreply.github.com>
2022-09-18 23:12:31 -0400

    proper way to create a post request

--- botometer.js
+++ botometer.js
@@ -1,18 +1,49 @@
-const { Botometer } = require("botometer");
- 
-const botometer = new Botometer({
-  consumerKey: "aB7JYAQGSJ0N8RSeUepRnpkpo",
-  consumerSecret: "K9GriFk0iTsGihEODTpHhtqCeZoeuj4suwFskn2ghuecjrfeps",
-  accessToken: "1569307940443545602-szyycVG6K3az9evyWmxQeomjJo6Dnb",
-  accessTokenSecret: "J73I10obcpBX4QiDmW2H315JW4TF8w06rxVWwxSxdQlhM",
-  rapidApiKey: "ae92546582mshacaa08852bc7c87p135209jsn655208c2edca",
-  supressLogs: false, // Not required. Defaults to true
-});
- 
-async function run() {
-  const results = await botometer.getScores(["@aandreyluiz"]);
- 
-  console.log(results);
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+
+export default function App() {
+
+    let [score, setScore] = React.useState('')
+    let [handle, setHandle] = React.useState('')
+
+    const fetchApiCall = () => {
+      fetch("https://botometer-pro.p.rapidapi.com/4/check_account", {
+        "method": "POST",
+        "headers": {
+          "x-rapidapi-host": "botometer-pro.p.rapidapi.com",
+          "x-rapidapi-key": "ae92546582mshacaa08852bc7c87p135209jsn655208c2edca"
+        },
+        "User" : {
+          "screen_name": "ElonMusk"}
+      })
+        .then(response => response.json())
+        .then(response => {
+          console.log(response.content);
+          console.log(response.originator.name)
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    return (
+      <View style={styles.container}>
+        <Text>Twitter Bot Detector!</Text>
+        <TouchableHighlight onPress={fetchApiCall}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Use Fetch API</Text>
+          </View>
+        </TouchableHighlight>
+        <StatusBar style="auto" />
+      </View>
+    );
 }
- 
-run();
+
+const styles = StyleSheet.create({
+  container: {
+    fontSize: 35,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
